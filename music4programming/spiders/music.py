10d78b41471e67649af4148b09180a7f0582b753
cothan <cothannguyen@gmail.com>
2017-04-06 13:49:34 +0700

    mypipeline

--- music4programming/spiders/music.py
+++ music4programming/spiders/music.py
@@ -19,12 +19,12 @@ class MyFilesPipeLine(FilesPipeline):
 		self.arg = arg
 		
 	def get_media_requests(self, item, info):
-		for url in item['file_urls']:
-			meta = {'filename': item['title']}
-			yield scrapy.Request(url, meta=meta)
+		url = item['file_urls']
+		meta = {'filename': item['title']}
+		yield scrapy.Request(url=url, meta=meta)
 
 	def file_path(self, request, response=None, info=None):
-		return '{}.mp3'.format(request.meta['title'])
+		return request.meta.get('title', '')
 
 
 class MusicSpider(scrapy.Spider):
