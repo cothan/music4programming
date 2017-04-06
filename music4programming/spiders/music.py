d0bc457ea8a4500fdddcc42601064e3a25352676
cothan <cothannguyen@gmail.com>
2017-04-06 12:09:28 +0700

    new file:   music4programming/__init__.py 	new file:   music4programming/__init__.pyc 	new file:   music4programming/items.py 	new file:   music4programming/middlewares.py 	new file:   music4programming/pipelines.py 	new file:   music4programming/pipelines.pyc 	new file:   music4programming/settings.py 	new file:   music4programming/settings.pyc 	new file:   music4programming/spiders/__init__.py 	new file:   music4programming/spiders/__init__.pyc 	new file:   music4programming/spiders/music.py 	new file:   music4programming/spiders/music.pyc 	new file:   scrapy.cfg

---
+++ music4programming/spiders/music.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#from scrapy.http.request import Request
+import scrapy
+
+
+class Songs(scrapy.Item):
+	"""docstring for Songs"""
+	#def __init__(self, arg):
+		#super(Songs, self).__init__()
+	title = scrapy.Field()
+	file_urls = scrapy.Field()
+	files = scrapy.Field()
+	
+
+class MusicSpider(scrapy.Spider):
+	name = "music"
+	allowed_domains = ["musicforprogramming.net"]
+	start_urls = ['http://musicforprogramming.net/']
+
+	count = 0 
+
+	def parse(self, response):
+		# The name alone contains u'musicForProgramming("47: Abe Mangger");'
+		# So we strip out to get the name of the song only
+		title = response.css('title::text').extract_first()[len('musicForProgramming("'):-3]
+		src = response.xpath('//audio[@id="player"]/@src').extract_first() 
+
+		self.count += 1
+		if self.count >= 46:
+			return
+
+		yield Songs(title=title, file_urls=[src])
+
+		next_url = response.xpath('//div[@id="episodes"]/a/@href').extract()[self.count]
+		
+		if next_url is not None:
+			yield scrapy.Request(response.urljoin('http://musicforprogramming.net/' + next_url))
+			
+
+
+		
